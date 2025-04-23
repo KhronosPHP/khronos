9cf2bbc665a6b74af281ea402e07c66888003847
Aidan Casey <aidan@caseyhouse.net>
2025-04-23 17:17:29 -0400

    test(event-sourcing): add test for ParameterTypeEventApplier

---
+++ packages/event-sourcing/tests/Unit/Fixtures/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Khronos\EventSourcing\Tests\Unit\Fixtures;
+
+use Khronos\EventSourcing\AggregateRoot;
+use Khronos\EventSourcing\RecordsEvents;
+
+final class Book implements AggregateRoot
+{
+    use RecordsEvents;
+
+    public bool $checkedOut = false;
+
+    public function applyBookWasCheckedOut(BookWasCheckedOut $event): void
+    {
+        $this->checkedOut = true;
+    }
+}
